daf2656bafa91ef5af4446e98ab520fdb3e086bf
lexasss <oleg.spakov@tuni.fi>
2023-08-22 09:43:44 +0300

    checking carla and srunner root folders

--- .set-env.ps1
+++ .set-env.ps1
@@ -1,3 +1,16 @@
-$env:CARLA_ROOT = 'C:\Users\olequ\Downloads\CARLA 0.9.13'
-$env:SCENARIO_RUNNER_ROOT = 'C:\Users\olequ\Downloads\CARLA 0.9.13\scenario_runner-0.9.13'
+$carla = 'C:\Users\olequ\Downloads\CARLA 0.9.13'
+$srunner = 'C:\Users\olequ\Downloads\CARLA 0.9.13\scenario_runner-0.9.13'
+
+if (-Not (Test-Path -Path $carla)) {
+    "CARLA's root folder specified in `.enter-env.ps1` is not valid."
+    exit
+}
+
+if (-Not (Test-Path -Path $carla)) {
+    "SRunner's root folder specified in `.enter-env.ps1` is not valid."
+    exit
+}
+
+$env:CARLA_ROOT = $carla
+$env:SCENARIO_RUNNER_ROOT = $srunner
 $env:PYTHONPATH = ";$env:CARLA_ROOT\PythonAPI\carla;$env:CARLA_ROOT\PythonAPI\carla\dist\carla-0.9.13-py3.7-win-amd64.egg"
