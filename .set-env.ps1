271d3c3196b817c10a5b14e055efdae962aa8768
John Mäkelä <john.makela@tuni.fi>
2023-08-22 11:36:44 +0300

    lab pathes

--- .set-env.ps1
+++ .set-env.ps1
@@ -1,6 +1,11 @@
 $carla = 'C:\Users\olequ\Downloads\CARLA 0.9.13'
 $srunner = 'C:\Users\olequ\Downloads\CARLA 0.9.13\scenario_runner-0.9.13'
 
+if ($env:computername -eq 'DESKTOP-6F2MBAF') {
+	$carla = 'D:\CarlaGit\carla\Build\UE4Carla\0.9.13-30-g9494e656e\WindowsNoEditor'
+	$srunner = 'D:\scenario_runner-0.9.13'
+}
+
 if (-Not (Test-Path -Path $carla)) {
     "CARLA's root folder specified in `.enter-env.ps1` is not valid."
     exit
